055f46840d32603aea60b26bc32f945ddd45d737
abhishekoo11oo <ayushmal4056@gmail.com>
2026-09-05 22:02:54 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  // output: "standalone",
   allowedDevOrigins: ["127.0.0.1", "localhost"],
     typescript: {
     ignoreBuildErrors: true,
